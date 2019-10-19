2a255e697b7e66d24d675d703d6a95be1ddb6b9b
aahmed019 <aliproahmed@gmail.com>
2019-10-18 20:06:27 -0400

    temp footer

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" 
+      integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" 
+      crossorigin="anonymous"/>
+          <link rel="stylesheet" href="styles.css"></link>
+
+      <footer>
+      <a id ="profile-link" href = "" target ="_blank" className = "btn contact-details"><i className ="fab fa-github"></i> GitHub</a>
+      
+      <a id ="facebook" href = "" target ="_blank" className = "btn contact-details"><i className ="fab fa-facebook"></i> Facebook</a>
+    
+    
+      <a id ="instagram" href = "" target ="_blank" className = "btn contact-details"><i className ="fab fa-instagram" id = "inst-logo"></i> Instagram</a>
+    
+  
+      <a id ="freecodecamp" href = "" target ="_blank" className = "btn contact-details"><i className ="fab fa-free-code-camp"></i> FreeCodeCamp</a>
+      
+      <a id ="LinkedIn" href = "" target ="_blank" className = "btn contact-details"><i className ="fab fa-linkedin"></i> LinkedIn</a>
+
+      </footer>
     </div>
   );
 }
