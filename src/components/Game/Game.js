47c1d40205211af2e354338ee1f086c8428c57f3
ltran0817 <lam.tran0896@gmail.com>
2019-11-02 14:27:38 -0400

    add game.json file

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import games from './games.json';
 
 class Game extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      index: 0,
+      questionIndex: 1,
+      hintCount: 0,
       latitude: null,
       longitude: null,
     }
@@ -12,11 +16,11 @@ class Game extends Component {
 
   position = async () => {
     await navigator.geolocation.getCurrentPosition(
-      position => this.setState({ 
-        latitude: position.coords.latitude, 
+      position => this.setState({
+        latitude: position.coords.latitude,
         longitude: position.coords.longitude
       }), newState => console.log(newState))
-      
+
     console.log(this.state.latitude, this.state.longitude)
   }
 
@@ -25,39 +29,44 @@ class Game extends Component {
 
     return (
       <div className="Game">
-        <section className = "middle">
-        <body>
-    
-      <div className="exit">
-    <p>Nav should be here</p>
-    <button className="btn-large btn-danger" type="button">&nbsp; Exit &nbsp;</button>
-    </div>
-    <div className ="text-center">
-    <p className="text-center">Question{/*states for question*/}</p>
-    
-    <br/>
-    <br/>
-    <img className = "" src = "https://static01.nyt.com/images/2016/05/28/nyregion/29CUNY1/29CUNY1-articleLarge.jpg?quality=75&auto=webp&disable=upscale"/>
-    <br/>
-    <br/>
-    <br/>
-    <input type="text" className ="text-center textbox"/>
-    <br/>
-    <br/>
-    <div >
-    <button className="btn-large  btn-success" type="button">&nbsp; Answer &nbsp;</button>
-    <br/>
-    <br/>
-    <button className="btn-large btn-warning " type="button">Hint</button>
-    </div>
-    </div>
+        <section className="middle">
+          <body>
 
-      <p className="cr text-center"><strong>Escape Team © 2019</strong></p>
+            <div className="exit">
+              <p>Nav should be here</p>
+              <button className="btn-large btn-danger" type="button">&nbsp; Exit &nbsp;</button>
+            </div>
+            <div className="text-center">
+              <h1 className="gameTitle">
+                {games[this.state.index].Title} Challenge
+              </h1>
+              <p className="text-center">
+                Question {this.state.questionIndex}
+              </p>
+              <p className="text-center">
+                {games[this.state.index].questions[this.state.questionIndex]}
+              </p>
+              <br />
+              <img className="" src="https://static01.nyt.com/images/2016/05/28/nyregion/29CUNY1/29CUNY1-articleLarge.jpg?quality=75&auto=webp&disable=upscale" />
+              <br />
+              <br />
+              <input id="answer" type="text" className="text-center textbox" />
+              <br />
+              <br />
+              <div >
+                <button className="btn-large  btn-success" type="button">&nbsp; Submit &nbsp;</button>
+                <br />
+                <br />
+                <button className="btn-large btn-warning " type="button">Hint</button>
+              </div>
+            </div>
 
-  </body>
-      </section>
-          
-        </div>
+            <p className="cr text-center"><strong>Escape Team © 2019</strong></p>
+
+          </body>
+        </section>
+
+      </div>
     );
   }
 }
