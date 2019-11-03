db7f7fea8327735920fbc7862eeefb9054a899bf
ltran0817 <lam.tran0896@gmail.com>
2019-11-03 00:13:23 -0400

    add timer on hint button

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -76,6 +76,11 @@ class Game extends Component {
     else {
       hintArea.innerText = "Sorry You've Run Out Of Hint! NOW USE YOUR DAMN BRAIN"
     }
+    document.getElementById("hintBttn").disabled = true;
+    // document.getElementById("hintBttn").style.backgroundColor = "gray";
+    setTimeout(function () {
+      document.getElementById("hintBttn").disabled = false;
+    }, 4000)
   }
 
   navbar = props => (
@@ -127,8 +132,8 @@ class Game extends Component {
           crossOrigin="anonymous" />
         <div>
           {/* <this.navbar drawerClickHandler={this.drawerToggleClickHandler} /> */}
-          <Navbar drawerClickHandler = {this.drawerToggleClickHandler}/>
-          <SideDrawerMenu show = {this.state.sideDrawerMenuOpen} />
+          <Navbar drawerClickHandler={this.drawerToggleClickHandler} />
+          <SideDrawerMenu show={this.state.sideDrawerMenuOpen} />
           {backdrop}
         </div>
         <section className="middle">
@@ -152,7 +157,7 @@ class Game extends Component {
 
               <br />
               <br />
-              <img className="" src= {games[this.state.index].images[this.state.imageIndex]} alt = "puzzle" />
+              <img className="" src={games[this.state.index].images[this.state.imageIndex]} alt="puzzle" />
               <br />
               <br />
               <br />
@@ -167,7 +172,7 @@ class Game extends Component {
                 <br />
                 <br />
 
-                <button className="btn-large btn-warning " type="button" onClick={this.getHint}>
+                <button id="hintBttn" className="btn-large btn-warning " type="button" onClick={this.getHint}>
                   {games[this.state.index].total_hint - this.state.hintCount} Hint(s) Left</button>
               </div>
             </div>
