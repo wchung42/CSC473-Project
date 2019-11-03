635c13ffcfac0553fa4b8e4c9cf49c1563791138
william <willchung42@gmail.com>
2019-11-03 13:25:13 -0500

    moved game to a puzzle component

--- src/App.js
+++ src/App.js
@@ -82,34 +82,8 @@ class App extends Component {
           </body>
         </section>
 
-        <footer className="footer-all">
-          <div className="footer-social">
 
-            <a href="#"><i className="fab fa-facebook"></i></a>
-            <a href="#"><i className="fab fa-twitter"></i></a>
-            <a href="#"><i className="fab fa-linkedin"></i></a>
-            <a href="#"><i className="fab fa-github"></i></a>
-
-          </div>
-
-          <div className="footer-info">
-
-            <p>
-              <a href="#">Home</a>
-              ·
-					<a href ="#">Games</a>
-              ·
-					<a href="#">About</a>
-              ·
-					<a href="#">Faq</a>
-              ·
-					<a href="#">Contact</a>
-            </p>
-
-            <p><strong>Escape Team © 2019</strong></p>
-          </div>
-
-        </footer>
+        {/* replace with footer component */}
       </div>
     );
   }
