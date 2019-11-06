f877e0deb781c1ac8356fb762ea0c24c5915a98e
ltran0817 <lam.tran0896@gmail.com>
2019-11-06 00:21:02 -0500

    Add the feature to dynamically choose game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -10,8 +10,19 @@ class Game extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      gameID: 0,
+      gameReady: false,
+    };
+    this.getGameId = this.getGameId.bind(this);
+  }
 
-    }
+  getGameId(ev) {
+    console.log(ev.currentTarget.value)
+    this.setState({
+      gameID: ev.currentTarget.value,
+      gameReady: true
+    }, console.log(this.state.gameID))
+    console.log(this.state.gameID)
   }
 
   // position = async () => {
@@ -26,26 +37,47 @@ class Game extends Component {
 
   //Want to load the game in here based on the name
   render = () => {
+    if (!this.state.gameReady) {
+      return (
+        <div className="Game">
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+            integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+            crossOrigin="anonymous" />
+          <br />
+          <div className="exit">
+            <button className="btn-large btn-danger" type="button"><a href="http://localhost:3000/home">&nbsp; Exit &nbsp;</a></button>
+          </div>
+          <div>
+            <button id="bttn1" type="button" onClick={this.getGameId} value="0">0</button>
+            <button id="bttn2" type="button" onClick={this.getGameId} value="1">1</button>
+            <button id="bttn3" type="button" onClick={this.getGameId} value="2">2</button>
+            <button id="bttn4" type="button" onClick={this.getGameId} value="3">3</button>
+          </div>
+          <br />
+        </div>
+      )
+    }
+    else {
+      return (
+        <div className="Game">
 
-    return (
-      <div className="Game">
-
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
-          integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
-          crossOrigin="anonymous" />
-        <br />
-        <div className="exit">
-          <button className="btn-large btn-danger" type="button"><a href ="http://localhost:3000/home">&nbsp; Exit &nbsp;</a></button>
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+            integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+            crossOrigin="anonymous" />
+          <br />
+          <div className="exit">
+            <button className="btn-large btn-danger" type="button"><a href="http://localhost:3000/home">&nbsp; Exit &nbsp;</a></button>
+          </div>
+          <div>
+            <Timer startCount="300" />
+          </div>
+          <div className="game-state">
+            <Puzzle gameId={this.state.gameID} />
+          </div>
+          <br />
         </div>
-        <div>
-          <Timer startCount = "300" />
-        </div>
-        <div className = "game-state">
-          <Puzzle />
-        </div>
-        <br />
-      </div>
-    );
+      );
+    }
   }
 }
 
--- src/components/Game/Puzzle.js
+++ src/components/Game/Puzzle.js
@@ -8,58 +8,58 @@ class Puzzle extends Component {
     constructor(props) {
         super(props)
         this.state = {
-          index: 0,
-          questionIndex: 1,
-          // image change
-          imageIndex: 1,
-          hintCount: 0,
-          usedHint: false,
-          latitude: null,
-          longitude: null,
-          // game ends when last question is completed
-          gameState: true,
-          win: false
+            index: this.props.gameId,
+            questionIndex: 1,
+            // image change
+            imageIndex: 1,
+            hintCount: 0,
+            usedHint: false,
+            latitude: null,
+            longitude: null,
+            // game ends when last question is completed
+            gameState: true,
+            win: false
         }
         this.getAnswer = this.getAnswer.bind(this);
         this.getHint = this.getHint.bind(this);
     }
-    
+
     getAnswer() {
         let localIndex = this.state.index;
         let localQuestionIndex = this.state.questionIndex;
         let qIndex = localQuestionIndex + 1;  //INDICATE THE NEXT QUESTION
         let imgIndex = qIndex; // IMAGE INDEX 
-        let answer = games[localIndex].answers[localQuestionIndex].toLowerCase();
+        let answer = games[localIndex].answers[localQuestionIndex];
         let answerBox = document.getElementById("answer");
         let userAnswer = answerBox.value; //USER ANSWER = VALUE OF ANSWER BOX
         // console.log(answer);
-        
-       
-        if (userAnswer == answer) {
-          document.getElementById("result").innerText = "Correct";
-          answerBox.style.borderColor = "palegreen";
-          answerBox.value = "";
-          document.getElementById("hint").innerText ="";
-          this.setState({
-            questionIndex: qIndex,
-            imageIndex: imgIndex,
-            usedHint: false
-          })
-          // check if for more questions
-          if (localQuestionIndex == games[localIndex].total_questions) {
+
+
+        if (userAnswer.toLowerCase() == answer) {
+            document.getElementById("result").innerText = "Correct";
+            answerBox.style.borderColor = "palegreen";
+            answerBox.value = "";
+            document.getElementById("hint").innerText = "";
             this.setState({
-                gameState: false,
-                win: true
+                questionIndex: qIndex,
+                imageIndex: imgIndex,
+                usedHint: false
             })
-            console.log("End of game");
-        }
-          // console.log(this.state.questionIndex);
+            // check if for more questions
+            if (localQuestionIndex == games[localIndex].total_questions) {
+                this.setState({
+                    gameState: false,
+                    win: true
+                })
+                console.log("End of game");
+            }
+            // console.log(this.state.questionIndex);
         }
         else {
-          answerBox.style.borderColor = "salmon";
-          document.getElementById("result").innerText = "Wrong";
-          answerBox.value = "";
-          // console.log(this.state.questionIndex);
+            answerBox.style.borderColor = "salmon";
+            document.getElementById("result").innerText = "Wrong";
+            answerBox.value = "";
+            // console.log(this.state.questionIndex);
         }
         if (qIndex > games[localIndex].total_questions) {
             console.log("Submit button time out not necessary");
@@ -67,11 +67,11 @@ class Puzzle extends Component {
             document.getElementById("submitBttn").disabled = true;
             // document.getElementById("hintBttn").style.backgroundColor = "gray";
             setTimeout(function () {
-            document.getElementById("submitBttn").disabled = false;
+                document.getElementById("submitBttn").disabled = false;
             }, 2000)
         }
-      }
-    
+    }
+
     getHint() {
         let localIndex = this.state.index;
         let localQuestionIndex = this.state.questionIndex;
@@ -83,8 +83,8 @@ class Puzzle extends Component {
             hintCount += 1;
             hintArea.innerText = games[localIndex].hint[localQuestionIndex];
             this.setState({
-            hintCount: hintCount,
-            usedHint: true
+                hintCount: hintCount,
+                usedHint: true
             })
         }
         else if (usedHint) {
@@ -104,49 +104,49 @@ class Puzzle extends Component {
         }
     }
 
-    render () {
+    render() {
         // game states - playing or end game
-        if ( !this.state.gameState ) {
+        if (!this.state.gameState) {
             return (
                 <div>
-                    <Endgame outcome = {this.state.win}/>;
+                    <Endgame outcome={this.state.win} />;
                 </div>
             )
         }
         return (
-            <div className = "game">
-                    <section className="middle">
+            <div className="game">
+                <section className="middle">
 
-                        <div className="text-center">
-                        <br/>
+                    <div className="text-center">
+                        <br />
                         <h1 className="gameTitle">
                             {games[this.state.index].Title} Challenge
                         </h1>
-                        <br/>
+                        <br />
                         <p className="text-center quest">
                             {games[this.state.index].Game_Story[this.state.questionIndex].replace("\\n", "\n")}
                         </p>
 
                         <br /><br />
-                        <img className = "imgG" src= {games[this.state.index].images[this.state.imageIndex]}/>
+                        <img className="imgG" src={games[this.state.index].images[this.state.imageIndex]} />
                         <br /><br /><br />
                         <input id="answer" type="text" className="text-center textbox" />
-                        <p id="hint" className="questN" value =""></p>
+                        <p id="hint" className="questN" value=""></p>
                         <div>
                             <button id="submitBttn" className="btn-large  btn-success" type="button" onClick={this.getAnswer}>&nbsp; Submit &nbsp;</button>
                             <p id="result" className="questN"></p>
-                            
+
 
                             <button id="hintBttn" className="btn-large btn-warning " type="button" onClick={this.getHint}>
-                            {games[this.state.index].total_hint - this.state.hintCount} Hint(s) Left</button>
+                                {games[this.state.index].total_hint - this.state.hintCount} Hint(s) Left</button>
                         </div>
-                        </div>
-                    </section>
+                    </div>
+                </section>
             </div>
-        
+
         )
     }
-    
+
 
 }
 
