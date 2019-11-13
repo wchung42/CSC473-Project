617cdad969a43a0f53a3f3ac40d0e366efc6bd47
william <willchung42@gmail.com>
2019-11-12 23:29:47 -0500

    unit tested game.js

--- src/components/Game/__test__/Game.test.js
+++ src/components/Game/__test__/Game.test.js
@@ -24,4 +24,40 @@ describe("component renders with only one game div", () => {
 
 })
 
+describe("component renders different pages based on states", () => {
+    let wrapper;
+    beforeEach(() => { wrapper = shallow(<Game />); });
+    
+    // renders game list
+    it('renders div with className game-list', () => {
+        wrapper.setState( {
+            gameReady: false,
+            gameSynopsis: 0,
+            gameStart: 0,
+        });
+        wrapper.update();
+        expect(wrapper.find('div.game-list').length).toBe(1);
+    });
+
+    // renders synopsis
+    it('renders div with className synopsis', () => {
+        wrapper.setState( {
+            gameReady: true,
+            gameSynopsis: 1,
+            gameStart: 0,
+        });
+        wrapper.update();
+        expect(wrapper.find('div.synopsis').length).toBe(1);
+    });
+
+    it('renders div with className gameInterface', () => {
+        wrapper.setState ( {
+            gameReady: true,
+            gameSynopsis: 0,
+            gameStart: 1,
+        });
+        wrapper.update();
+        expect(wrapper.find('div.gameInterface').length).toBe(1);
+    })
+})
 
