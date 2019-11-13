eaf174e0e0fa2d7200b23f5255258414157c5903
yifang he <yifang1125@gmail.com>
2019-11-12 23:03:11 -0500

    more testing

--- src/util.test.js
+++ src/util.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from './App';
+import Game from './components/Game/Game';
 import renderer from 'react-test-renderer';
 
 
@@ -8,6 +9,35 @@ test('backdropClickHandler Testing',()=>{
   
     component.backdropClickHandler();
   
-    expect(component.state.data).toBeFalsy;
+    expect(component.state.sideDrawerMenuOpen).toBeFalsy;
   
   })
+
+  test('drawerToggleClickHandler Testing',()=>{
+    let component=renderer.create(<App />).getInstance();
+
+    component.drawerToggleClickHandler();
+
+    expect(component.state.sideDrawerMenuOpen).toBe(true);
+
+  })
+
+  test('getGameId',()=>{
+    let component=renderer.create(<Game />).getInstance();
+
+    component.getGameId();
+
+    expect(component.state.gameID).toBe(1)
+
+  })
+
+  test('startGame',()=>{
+    let component=renderer.create(<Game />).getInstance();
+
+    component.startGame();
+
+    expect(component.state.gameSynopsis).toBe(0);
+    expect(component.state.gameStart).toBe(1);
+
+  }
+  )
