3b4731a24661121308eb82ead869d1c8a8b92328
william <willchung42@gmail.com>
2019-11-13 11:22:05 -0500

    more puzzle.js testing

--- src/components/Game/__test__/Puzzle.test.js
+++ src/components/Game/__test__/Puzzle.test.js
@@ -3,10 +3,11 @@ import Puzzle from './../Puzzle';
 import Endgame from './../Endgame';
 import Question from './../Question';
 import Answer from './../Answer';
+import Timer from './../Timer';
 import { shallow, mount } from 'enzyme';
 
 describe("Puzzle component", () => {
-    it("Time component renders", () => {
+    it("Puzzle component renders", () => {
         const wrapper = mount(<Puzzle gameId = {0}/>);
         expect(wrapper.exists()).toBe(true);
     })
@@ -24,5 +25,16 @@ describe("Puzzle component", () => {
             expect(wrapper.find(Question).length).toBe(1);
             expect(wrapper.find(Answer).length).toBe(1);
         })
+    })
+
+    it("Timer stops upon win condition", () => {    
+        const mock = jest.fn();
+        const wrapper = shallow(<Puzzle gameId = {0} gameHandler = {mock}/>);
+        wrapper.setState({
+            win: true,
+        })
+        const wrapperInstance = wrapper.instance();
+        wrapperInstance.componentDidUpdate();
+        expect(wrapper.state('timeStopper')).toBe(1);
     })
 })
