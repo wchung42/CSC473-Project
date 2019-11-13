6ac298831c3e1e58ab7dd110cf55f5594e57bb0f
yifang he <yifang1125@gmail.com>
2019-11-13 11:06:43 -0500

    navbar testing

--- src/util.test.js
+++ src/util.test.js
@@ -2,42 +2,36 @@ import React from 'react';
 import App from './App';
 import Game from './components/Game/Game';
 import renderer from 'react-test-renderer';
+import ReactDOM from 'react-dom';
+import Puzzle from './components/Game/Puzzle'
+import Navbar from './components/Navbar/Navbar'
+
+import { shallow, mount } from 'enzyme';
+import { isTSAnyKeyword } from '@babel/types';
 
 
 test('backdropClickHandler Testing',()=>{
-    let component=renderer.create(<App />).getInstance();
+  let component=renderer.create(<App />).getInstance();
+
+  component.backdropClickHandler();
+
+  expect(component.state.sideDrawerMenuOpen).toBeFalsy;
+
+})
+test('drawerToggleClickHandler Testing',()=>{
+  let component=renderer.create(<App />).getInstance();
+
+  component.drawerToggleClickHandler();
+
+  expect(component.state.sideDrawerMenuOpen).toHaveReturned;
+
+})
+
+test('navbar Testing', () => {
+  // Render a checkbox with label in the document
+  const nav= shallow(<Navbar labelOn="On" labelOff="Off" />);
+
+  expect(nav.text()).toEqual('<drawerToggleButton />');
+
   
-    component.backdropClickHandler();
-  
-    expect(component.state.sideDrawerMenuOpen).toBeFalsy;
-  
-  })
-
-  test('drawerToggleClickHandler Testing',()=>{
-    let component=renderer.create(<App />).getInstance();
-
-    component.drawerToggleClickHandler();
-
-    expect(component.state.sideDrawerMenuOpen).toBe(true);
-
-  })
-
-  test('getGameId',()=>{
-    let component=renderer.create(<Game />).getInstance();
-
-    component.getGameId();
-
-    expect(component.state.gameID).toBe(1)
-
-  })
-
-  test('startGame',()=>{
-    let component=renderer.create(<Game />).getInstance();
-
-    component.startGame();
-
-    expect(component.state.gameSynopsis).toBe(0);
-    expect(component.state.gameStart).toBe(1);
-
-  }
-  )
+});
