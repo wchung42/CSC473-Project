8bd339bd86e8176ca00718fd0b1130ec3b72eedd
aahmed019 <aliproahmed@gmail.com>
2019-11-17 23:31:17 -0500

    get current location

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -12,6 +12,8 @@ class Game extends Component {
     this.state = {
       gameID: 0,
       gameReady: false,
+      latitude: null,
+      longitude: null,
       gameSynopsis: 0, // 0: don't display game synopsis ; 1: display synopsis
       gameStart: 0 // 0: start button clicked, start game ; 1: stay on synopsis page
     };
@@ -37,15 +39,20 @@ class Game extends Component {
     })
   }
 
-  // position = async () => {
-  //   await navigator.geolocation.getCurrentPosition(
-  //     position => this.setState({
-  //       latitude: position.coords.latitude,
-  //       longitude: position.coords.longitude
-  //     }), newState => console.log(newState))
+  position = async () => {
+    await navigator.geolocation.getCurrentPosition(
+       position => this.setState({
+        latitude: position.coords.latitude,
+         longitude: position.coords.longitude
+       }), newState => console.log(newState))
 
-  //   console.log(this.state.latitude, this.state.longitude)
-  // }
+       if(this.state.longitude === null){
+     console.log(this.state.latitude, this.state.longitude);
+       }
+       else{
+        console.log(this.state.latitude, this.state.longitude);
+       }
+   }
 
   //Want to load the game in here based on the name
   render = () => {
@@ -54,6 +61,12 @@ class Game extends Component {
     if (!this.state.gameReady && (this.state.gameSynopsis === 0) && (this.state.gameStart === 0)) {
       return (
         <div className="Game">
+          <br/>
+          <p>Click the button to get your coordinates.</p>
+
+          <button onClick={this.position} className='Filter'>Location</button>
+          <br/>
+
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
             integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
             crossOrigin="anonymous" />
