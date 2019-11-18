330c1cd4acfb35ea25509c5aea453feb24ef26fb
william <willchung42@gmail.com>
2019-11-18 18:04:38 -0500

    drag and drop items are now horizontal

--- src/components/Game/OrderQuestion/DragImage.js
+++ src/components/Game/OrderQuestion/DragImage.js
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 import { Draggable } from  'react-beautiful-dnd';
 
 const Container = styled.div`
-    border: 1px solid blue;
+    border: 1px solid black;
     border-radius: 2px;
     padding: 10px;
-    margin-bottom: 10px;
+    margin: 0 auto;
     background-color: white;
+    display: flex;
+    justify-content: center;
 `;
 
 class DragImage extends Component {
