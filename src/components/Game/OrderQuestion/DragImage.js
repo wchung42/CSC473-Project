fc665c5be5e0eb07ea9bfcd54d5c5542d60855b9
william <willchung42@gmail.com>
2019-11-18 17:39:27 -0500

    drag and drop working; todo: preserve reorder

--- src/components/Game/OrderQuestion/DragImage.js
+++ src/components/Game/OrderQuestion/DragImage.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { Draggable } from  'react-beautiful-dnd';
 
 const Container = styled.div`
     border: 1px solid blue;
     border-radius: 2px;
     padding: 10px;
     margin-bottom: 10px;
+    background-color: white;
 `;
 
 class DragImage extends Component {
     render() {
         return (
-            <Container>
-                <img src = { this.props.image.url } alt = { this.props.image.id }></img>
-            </Container>
-        )
+            <Draggable draggableId = { this.props.image.id } index = { this.props.index }>
+                {(provided) => (
+                    <Container
+                        // necessary draggable props --> makes each item draggable
+                        ref = {provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        
+                    >
+                        <img src = { this.props.image.url } alt = { this.props.image.id }></img>
+                    </Container>
+                )}
+              
+            </Draggable>
+            
+        );
     }
 }
 
