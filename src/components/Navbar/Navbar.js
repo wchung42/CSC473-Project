6588a60054ddf6ff182dbb5cb3ee9cd5c3609420
aahmed019 <aliproahmed@gmail.com>
2019-11-21 21:15:12 -0500

    added auth for game page, and regular home page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,126 @@
-import React from 'react';
-import './Navbar.css';
+import React, { Component } from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import Amplify, { Auth } from 'aws-amplify';
+import { Authenticator, Greetings } from 'aws-amplify-react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import awsconfig from '../../aws-exports';
+import '../Navbar/Navbar.css'
 import DrawerToggleButton from '../SideDrawerMenu/DrawerToggleButton';
-import { Auth } from 'aws-amplify';
 
-const navbar = props => (
-    <header >
-        <nav className="navigation navbar" >
+Amplify.configure(awsconfig);
+
+class Navigation extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          username: '',
+          showAuth: false,
+        };
+      }
+
+      async componentDidMount() {
+        const user = await Auth.currentUserInfo();
+        if (user) {
+          this.setState({
+            username: user.username,
+          });
+        }
+      }
+    
+      handleShowAuth = () => {
+        this.setState({
+          showAuth: true,
+        });
+      }
+    
+      handleCloseAuth = () => {
+        this.setState({
+          showAuth: false,
+        });
+      }
+    
+      handleAuthStateChange = (state) => {
+        if (state === 'signedIn') {
+          const { username } = Auth.user;
+          this.setState({
+            showAuth: false,
+            username,
+          });
+        }
+      }
+    
+      handleSignOut = () => {
+        Auth.signOut().then(() => {
+          this.setState({
+            username: '',
+          });
+        });
+      }
+    
+render(){
+
+
+      const { username, showAuth } = this.state;
+      const {
+        handleShowAuth, handleCloseAuth, handleAuthStateChange, handleSignOut,
+      } = this;
+
+
+return(
+   <div>
+        
+        <Navbar className="navigation navbar">
+         
             <div className="nav-logo"><a href="/"><img src={require("./escape.png")} alt="logo" /></a></div>
             <div className="space-btw-logo-items" />
              <div className="nav-items">
-                <ul>
-                    <li></li>
-                </ul>
-            </div> 
-            <div>
-                <DrawerToggleButton click={props.drawerClickHandler} />
+            {username
+              ? (                  
+                <div className ="userLOGOUT">
+                <Nav className="navigation navbar">
+                    <ul>
+                    <Nav.Item className="nav-link login" style={{color:'white'}}>
+                    <li>
+                      {' '}
+                      {username /*Printing the username*/} 
+                      </li>
+                    </Nav.Item>
+                  <Nav.Item>
+                    <Button className ="login" style={{color:'white', fontSize:'200%'}} onClick={handleSignOut} data-testid="logout-button"> <li style={{color:'white', fontSize:'100%'}}>Sign Out</li></Button>
+                  </Nav.Item>
+                  </ul>
+                </Nav>
+                </div>
+              )
+              : (
+                <Nav className="navigation navbar">
+                  <Button className ="login"
+                  style={{color:'white'}}
+                    onClick={handleShowAuth}
+                  >
+                    <a>Log in</a>
+                  </Button>
+                </Nav>
+              )}
+              </div>
+          
+          <div>
+                <DrawerToggleButton click={this.props.drawerClickHandler} />
             </div>
-        </nav>
-    </header>
-);
+        </Navbar>
 
-export default navbar;
+        <Dialog className ="auth" onClose={handleCloseAuth} aria-labelledby="simple-dialog-title" open={showAuth}>
+          <Authenticator
+            
+            hideDefault={!showAuth}
+            hide={[Greetings]}
+            onStateChange={handleAuthStateChange}
+          />
+        </Dialog>
+      </div>
+);
+}
+}
+export default Navigation;
