b77c7d54177cc225b4a3495f63a2a28d9d1bda68
william <willchung42@gmail.com>
2019-11-26 13:57:20 -0500

    changed locations; needs to be tested

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -137,8 +137,8 @@ class Game extends Component {
       // calculate distance to target
       dist = getDistanceFromLatLonInKm(userCoords.latitude, userCoords.longitude, target.latitude, target.longitude);
       console.log('Distance: ' + dist)
-      // player must be within 10 meters of starting point for game to begin
-      if (dist <= 0.09) {
+      // player must be within 50 meters of starting point for game to begin
+      if (dist <= 0.05) {
         console.log('You are here!');
         // stop watching player location
         navigator.geolocation.clearWatch(current)
