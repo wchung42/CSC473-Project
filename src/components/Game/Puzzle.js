2ef5b6c9a18bfe9001fc4142765b89456aae30e0
ltran0817 <lam.tran0896@gmail.com>
2019-11-26 13:00:04 -0500

    Puzzle file cleaned up completed

--- src/components/Game/Puzzle.js
+++ src/components/Game/Puzzle.js
@@ -23,7 +23,6 @@ class Puzzle extends Component {
             answers: this.props.gAnswers,
             hints: this.props.gHints,
             geoLocation: this.props.gGeoLocation,
-            // image change
             hintCount: 0,
             usedHint: false,
             latitude: null,
@@ -51,7 +50,7 @@ class Puzzle extends Component {
         console.log(answer)
         //if the answer is correct
         if (userAnswer.toLowerCase() == answer.toLowerCase()) {
-            //else moving to the next question
+            //moving to the next question
             console.log("right answer");
             // clear hint space when moving to next question
             document.getElementById('hint').innerText = '';
@@ -61,7 +60,7 @@ class Puzzle extends Component {
             })
             //if this is the last question then End game
             if (this.state.atQuestion == this.state.totalQuestions) {
-                this.setState({
+                await this.setState({
                     gameState: false,
                     win: true
                 }); console.log("End of game");
@@ -96,27 +95,30 @@ class Puzzle extends Component {
     }
 
     getHint() {
-        let localIndex = this.state.index;
-        let localQuestionIndex = this.state.atQuestion;
-        let totalHint = games[localIndex].Total_Hint;
+        let questionIndex = this.state.atQuestion;
+        let totalHint = this.state.totalHints;
         let hintCount = this.state.hintCount;
         let hintArea = document.getElementById("hint");
         let usedHint = this.state.usedHint;
+        //Display hint and increment hint count
         if (hintCount < totalHint && !usedHint) {
             hintCount += 1;
-            hintArea.innerText = games[localIndex].Hint[localQuestionIndex];
+            hintArea.innerText = this.state.hints[questionIndex];
             this.setState({
                 hintCount: hintCount,
                 usedHint: true
             })
         }
+        //when the player press the hint button more than once for same question
         else if (usedHint) {
-            hintArea.innerText = games[localIndex].Hint[localQuestionIndex];
+            hintArea.innerText = this.state.hints[questionIndex];
         }
+        //when the users run out of hint
         else {
             hintArea.innerText = "Sorry You've Run Out Of Hint! NOW USE YOUR DAMN BRAIN"
         }
-        if (localQuestionIndex + 1 > games[localIndex].total_questions) {
+        //Timeout to prevent spamming
+        if (questionIndex + 1 > this.state.totalQuestions) {
             console.log("Hint button timeout not necessary");
         } else {
             document.getElementById("hintBttn").disabled = true;
@@ -161,7 +163,7 @@ class Puzzle extends Component {
                             <p id="hint" className="questN" value=""></p>
                             <div className="hint">
                                 <button id="hintBttn" className="btn-lg btn-warning" type="button" onClick={this.getHint}>
-                                    {games[this.state.index].Total_Hint - this.state.hintCount} Hint(s) Left</button>
+                                    {this.state.totalHints - this.state.hintCount} Hint(s) Left</button>
                             </div>
                         </div>
                     </section>
