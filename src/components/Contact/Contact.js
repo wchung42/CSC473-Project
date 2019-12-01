364dd0893587fc045b84d2df205ac8566d36ce60
william <willchung42@gmail.com>
2019-12-01 16:28:19 -0500

    added emailjs api; contact page works now

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,36 +1,99 @@
-import React from 'react';
-import './Contact.css'
+import React, { Component } from 'react';
+import './Contact.css';
+// import email js api
+import * as emailjs from 'emailjs-com';
 
+class Contact extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+        }
+    }
+    
 
+    // handle submitting of form
+    handleSubmit(e) {
+        e.preventDefault();
 
-const Contact=(props)=>{
-    return(
-        <div className="container">
-            <div id = 'heading-title'>
-                <h1>Contact Us</h1> 
-            </div>
-            <div className = 'form-area was-validated'>
-                <form>
-                    <div className = 'form-group'>
-                        <label for = 'name'>Your Name (required)</label>
-                        <input type = 'text' className = 'form-control' required></input>
-                        <label for = 'name'>Your Email (required)</label>
-                        <input type = 'email' className = 'form-control' required></input>
-                        <label for = 'name'>Your Message (required)</label>
-                        <textarea className = 'form-control' rows = '5' required />
-                        {/* <div class = 'invalid-feedback'>
-                            Please enter a message.
-                        </div> */}
-                        <br></br>
-                        <button type = 'submit' className = 'btn-lg' id = 'submit-button'>Submit</button>
-                    </div>
-                </form>
-            </div>
+        const { name, email, subject, message } = this.state;
+
+        // create template for emailjs
+        let templateParams = {
+          from_name: name,
+          from_email: email,
+          to_name: 'Escape Team',
+          subject: subject,
+          message_html: message,
+         }
+
+         // send email
+         emailjs.send(
+          'gmail',
+          'template_WmqXIzdF',
+           templateParams,
+          'user_hcbgkj1kkrrFunOJqWNLQ'
+         ).then (function(response) {
+             console.log('Successfully sent email', response.status, response.text);
+         }, function(error) {
+             console.log('Failed to send', error);
+         });
+
+         this.resetForm()
+     }
+    
+    // reset form values
+    resetForm() {
+        this.setState({
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+        })    
+    }
+
+    // handle change
+    handleChange = (param, e) => {
+        console.log(this.state.name);
+        this.setState({
+            [param]: e.target.value,
+        })
         
-        </div>
-        
-        
-    )
+    }
+
+    render () {
+        return (
+            <div className="container">
+                <div id = 'heading-title'>
+                    <h1>Contact Us</h1> 
+                </div>
+                <div className = 'form-area was-validated'>
+                    <form onSubmit = { this.handleSubmit.bind(this) }>
+                        <div className = 'form-group'>
+                            <label for = 'name'>Your Name (required)</label>
+                            <input type = 'text' className = 'form-control' value = { this.state.name } required onChange = { this.handleChange.bind(this, 'name') }></input>
+                            <label for = 'name'>Your Email (required)</label>
+                            <input type = 'email' className = 'form-control' value = { this.state.email } required onChange = { this.handleChange.bind(this, 'email')}></input>
+                            <label for = 'name'>Subject (required)</label>
+                            <input type = 'text' className = 'form-control' value = { this.state.subject } required onChange = { this.handleChange.bind(this, 'subject')}></input>
+                            <label for = 'name'>Your Message (required)</label>
+                            <textarea className = 'form-control' rows = '5' value = { this.state.message } required onChange = { this.handleChange.bind(this, 'message') }/>
+                            {/* <div class = 'invalid-feedback'>
+                                Please enter a message.
+                            </div> */}
+                            <br></br>
+                            <button type = 'submit' className = 'btn-lg' id = 'submit-button'>Submit</button>
+                        </div>
+                    </form>
+                </div>   
+            </div>    
+        )
+         
+    }
+                   
 };
 
 export default Contact;
