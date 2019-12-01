47e9843c99ad3ff9c064ed1b21a361869da9ff81
william <willchung42@gmail.com>
2019-12-01 17:57:53 -0500

    styling about us

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './About.css'
 
-
-
 const About=(props)=>{
     return(
-        <div>
+        <div className = 'about-container'>
             
             <div><h1>About Us</h1></div>
             <div><a href="/"><img className="a-image"src={require("./escaperoom.jpg")} alt="logo" /></a></div>
