2e0c666025a5fc430d620c44a51db3713ec551da
yifang he <yifang1125@gmail.com>
2019-12-03 14:49:25 -0500

    contact page

--- src/components/Contact/Contact.test.js
+++ src/components/Contact/Contact.test.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Contact from './Contact'
+//import{finByTestArr}from './../util'
+
+// const findByTestAtrr=(component,attr)=>{
+//     const wrapper=component.find(attr);
+//     return wrapper;
+// }
+// const setUp=(props={})=>{
+
+//     const component=shallow(<Contact {...props} />);
+//     return component;
+    
+
+// };
 
 describe('contact page', () => {
     it('check that has navigation', () => {
@@ -32,4 +45,28 @@ describe('contact page', () => {
 
     })
 
+   
+
 });
+
+
+describe('Have props',()=>{
+    let wrapper
+   
+    it('enter message',()=>{
+             const prop={
+                    name:'aa',
+                    email:'bb',
+                    subject:'cc',
+                    message:'dd'
+             }
+             wrapper = shallow(<Contact {...prop} />);
+             expect(wrapper.find('div.form-group').length).toBe(1);
+    
+    
+    })
+    
+    
+
+});
+
