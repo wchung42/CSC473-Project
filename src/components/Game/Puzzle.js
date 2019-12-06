3c95ccc0bab7ad40e9257ea2acbc616776225b19
ltran0817 <lam.tran0896@gmail.com>
2019-12-06 12:39:01 -0500

    Revert "to test with timeout on phone"
    This reverts commit a270aad59e039d0b1a0c009f2c95fc6c0be28329.

--- src/components/Game/Puzzle.js
+++ src/components/Game/Puzzle.js
@@ -272,7 +272,7 @@ class Puzzle extends Component {
         }
 
         // start watching
-        current = setTimeOut(navigator.geolocation.watchPosition(success, error, { enableHighAccuracy: true }), 10000);
+        current = navigator.geolocation.watchPosition(success, error, { enableHighAccuracy: true });
     }
 
     componentWillUnmount() {
