157ab8a53bfd883036ca01babe986ec7033d2498
yifang he <yifang1125@gmail.com>
2019-12-08 23:21:46 -0500

    finish home render section

--- src/components/home/home.test.js
+++ src/components/home/home.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Home from './home'
+//import Navbar from './components/Navbar/Navbar'
 
 describe('<Home /> component testing', () => {
     describe("<Home /> renders correctly", () => {
@@ -13,5 +14,32 @@ describe('<Home /> component testing', () => {
             wrapper = shallow(<Home />);
             expect(wrapper.find('img.hp-image').length).toBe(1);
         });
+
+        it('renders out text in home page', () => {
+            wrapper = shallow(<Home/>);
+
+            expect(wrapper.find('div.banner-text').length).toBe(1);
+        
+        });
+
+        it('renders out play button',()=>{
+            wrapper=shallow(<Home />);
+
+            expect(wrapper.find('div#play-now-button').length).toBe(1);
+        })
+
+        it('renders out intro section',()=>{
+            wrapper=shallow(<Home />);
+
+            expect(wrapper.find('div#intro').length).toBe(1);
+        })
+
+        it('renders out mission-text section',()=>{
+            wrapper=shallow(<Home />);
+
+            expect(wrapper.find('div.mission-text').length).toBe(3);
+        })
+
+
     })
 })
