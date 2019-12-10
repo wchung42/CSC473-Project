bcd29df16a115edaa0e76bf0e9163be4c1f61d67
yifang he <yifang1125@gmail.com>
2019-12-10 18:19:00 -0500

    testing button child length

--- src/components/SideDrawerMenu/DrawerToggleButton.test.js
+++ src/components/SideDrawerMenu/DrawerToggleButton.test.js
@@ -5,11 +5,25 @@ import DrawerToggleButton from './DrawerToggleButton'
 
 describe('<DrawerToggleButton/> component testing', () => {
     let wrapper;
-    it("check button click", () => {
+    it("check button click render", () => {
         wrapper = shallow(<DrawerToggleButton/ >);
-        expect(wrapper.find('button.toggle-button').length).toBe(1)
-    
-            
+        expect(wrapper.find('button.toggle-button').length).toBe(1)        
 
     })
+
+    it("check button click render", () => {
+        wrapper = shallow(<DrawerToggleButton/ >);
+        expect(wrapper.find('button.toggle-button').length).toBe(1)        
+
+    })
+
+    it('testing button length', () => {
+
+        const wrapper= shallow(<DrawerToggleButton />);
+      
+        expect(wrapper.find('button').children().length).toBe(3); 
+    
+      })
+
+      
 })
