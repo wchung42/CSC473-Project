ba0c182a0fcc9ee96640daf00d0044cfc2778063
yifang he <yifang1125@gmail.com>
2019-12-10 19:17:56 -0500

    testing on parant class

--- src/components/SideDrawerMenu/SideDrawerMenu.test.js
+++ src/components/SideDrawerMenu/SideDrawerMenu.test.js
@@ -4,15 +4,27 @@ import SideDraw from './SideDrawerMenu'
 import sideDrawerMenu from './SideDrawerMenu';
 import { exportAllDeclaration } from '@babel/types';
 import DrawerToggleButton from './DrawerToggleButton'
+
+
 describe('<sideDrawerMenu/> component testing', () => {
     let wrapper;
     it("sideDraw child testing", () => {
        const DrawerToggle=shallow(<DrawerToggleButton/ >);
-        const wrapper = shallow(<sideDrawerMenu/ >);
+         wrapper = shallow(<sideDrawerMenu/ >);
         DrawerToggle.find('.toggle-button').simulate('click');
         //expect(wrapper.find('div.side-drawer-divider').length).toBe(1)
         expect(wrapper.hasClass('side-drawer-divider'))
             
 
     })
+    it("sideDraw parent class testing", () => {
+        const DrawerToggle=shallow(<DrawerToggleButton/ >);
+          wrapper = shallow(<sideDrawerMenu/ >);
+        // DrawerToggle.find('.toggle-button').simulate('click');
+         
+         expect(wrapper.hasClass('animateDrawer'))
+
+
+})
+
 })
